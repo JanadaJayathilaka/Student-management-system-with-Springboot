00e1c3a001fae8eb8ca9c2648910d95b4b6eca93
janada jayathilaka <86776703+JanadaJayathilaka@users.noreply.github.com>
2024-11-18 23:19:21 +0530

    tuto 3

--- demo1/src/main/java/com/example/demo1/controller/StudentController.java
+++ demo1/src/main/java/com/example/demo1/controller/StudentController.java
@@ -3,7 +3,6 @@ package com.example.demo1.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo1.model.Student;
 import com.example.demo1.service.StudentService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/students")
@@ -52,4 +52,28 @@ public class StudentController {
         studentService.deleteStudent(id);
         return new ResponseEntity<>("Student deleted successfully", HttpStatus.OK);
     }
+
+    @GetMapping("/yearOfEnrollment/{yearOfEnrollment}")
+    public ResponseEntity<List<Student>> getStudentByYearOfEnrollment(@PathVariable int yearOfEnrollment) {
+        List<Student> students = studentService.getStudentByYearOfEnrollment(yearOfEnrollment);
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/department/{id}")
+    public ResponseEntity<String> getDepartmentById(@PathVariable Long id) {
+        String departmentName = studentService.getDepartmentById(id);
+        if (departmentName != null) {
+            return new ResponseEntity<String>(departmentName, HttpStatus.OK);
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/removeStudent/{yearOfEnrollment}")
+    public ResponseEntity<String> deleteStudentByYearOfEnrollment(@PathVariable int yearOfEnrollment) {
+        studentService.removeStudentByYearOfEnrollment(yearOfEnrollment);
+        return ResponseEntity.ok("Student enrolled in" + yearOfEnrollment + "have been removed.");
+    }
+
 }
--- demo1/src/main/java/com/example/demo1/repository/StudentRepository.java
+++ demo1/src/main/java/com/example/demo1/repository/StudentRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo1.model.Student;
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByYearOfEnrollment(int yearOfEnrollment);
+
+    @Query("SELECT s.department FROM students s WHERE s.id = :id")
+    String findDepartmentById(Long id);
+
+    @Query("DELETE FROM students s WHERE s.yearOfEnrollment = :yearOfEnrollment")
+    void deleteByYearOfEnrollment(int yearOfEnrollment);
 
 }
